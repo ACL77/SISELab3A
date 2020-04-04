bbef7a7641181f8b6612abc297242fbbdab6890c
andrlourenco <andreandebol@gmail.com>
2020-04-04 14:19:10 +0100

    most recent (so i could delete from pc)

--- src/Counter.java
+++ src/Counter.java
@@ -1,15 +1,27 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 class Counter {
-    private int cnt = 0;
+    //atomic integer will not lock for reading values
+    private AtomicInteger cnt;
+    //private int cnt = 0;
     public Counter(int init) {
-        cnt = init;
+
+        //cnt = init;
+        cnt = new AtomicInteger(init);
     }
     public synchronized void increment() {
-        cnt++;
+        //cnt++;
+        //first increments, then returns
+        cnt.incrementAndGet();
     }
     public synchronized void decrement() {
-        cnt--;
+
+        //cnt--
+        //first decrements, then returns
+        cnt.decrementAndGet();
     }
     public synchronized int value() {
-        return cnt;
+
+        return cnt.get();
     }
 }
